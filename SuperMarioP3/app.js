f9a45f90efdc2ba2aa66c5012a3ee361ecc60ee6
SansanDevelop <pedros03@ucm.es>
2021-04-16 00:43:51 +0200

    Supero Mario Nes::includingMapViewPort

--- SuperMarioP3/app.js
+++ SuperMarioP3/app.js
@@ -1,75 +1,66 @@
-
 var game = function() {
 
-
-
-    var Q = window.Q = Quintus({audioSupporter:["mp3", "ogg"]})
-        .include("Sprites, Scenes, Input","2D","Anim","TMX")
-        // Maximize this game to whatever the size of the browser is
+    var Q = window.Q = Quintus()
+        .include(["Sprites", "Scenes", "Input", "2D", "UI", "Anim", "TMX"])
         .setup("myGame")
-        // And turn on default input controls and touch input (for UI)
         .controls();
 
-        Q.Sprite.extend("Mario", {
 
-            init: function (p) {
+    Q.Sprite.extend("Mario", {
 
-                this._super(p, {
-                    sheet: "marioR",
-                    x: 250,
-                    y: 250,
-                    frame: 0,
-                    scale: 3
+        init: function(p) {
+            this._super(p,{
+                sheet: "marioR",
+                x: 60,
+                y: 550,
+                frame: 11,
+                scale: 1.5
+            });
+            Q.input.on("left", this, function() { this.p.x -= 10; });
+            Q.input.on("right", this, function() { this.p.x += 10; });
+            Q.input.on("up", this, function() { this.p.y -= 10; });
+            Q.input.on("down", this, function() { this.p.y += 10; });
 
-                });
+        }
+    });
 
-            }
-
-        });
-
-        Q.Sprite.extend("OneUp", {
-
-            init: function (p) {
-
-                this._super(p, {
-                    asset: "1up.png",
-                    scale: 0.3
-
-                });
-
-            }
-
-        });
-
-
-    Q.load([ "mario_small.png","mario_small.json", "1up.png" , "bg.png","tiles.tmx","map2021.tmx"], function() {
-
-        Q.compileSheets("mario_small.png","mario_small.json");
-
-        Q.stageTMX(".tmx",stage)
-
-        Q.scene("level1", function(stage) {
-            stage.insert(new Q.Repeater(
-                {asset:"bg.png",speedX:0.5,speedY:0.5}
-                ));
-            mario = new Q.Mario();
-            mario2 = new Q.Mario();
-            mario2.p.y = 50;
-            mario.p.frame = stage.options.frame;
-
-            stage.insert(mario);
-            stage.insert(new Q.OneUp(), mario);
-            stage.add("viewport").follow(mario, {x: true , y:false});
-
-        });
-
-        Q.stageScene("level1",0,{frame: 0});
-
-
-
+    Q.Sprite.extend("OneUp", {
+        init: function(p) {
+            this._super(p,{
+                asset: "1up.png",
+                scale: 0.3,
+                x: 20,
+                y: -10
+            });
+        }
     });
 
 
+    Q.load([ "mario_small.png","mario_small.json", "1up.png", "bg.png", "mapa2021.tmx", "tiles.png" ], function() {
+
+        Q.compileSheets("mario_small.png","mario_small.json");
+
+        Q.scene("level1", function(stage) {
+            /*
+              stage.insert(
+                  new Q.Repeater({asset: "bg.png", speedX: 0.5, speedY: 0.5})
+              );
+              */
+            Q.stageTMX("mapa2021.tmx", stage);
+
+            mario = new Q.Mario();
+            stage.insert(mario);
+            //stage.insert(new Q.OneUp(), mario);
+
+            stage.add("viewport").follow(mario,{x:true, y:false});
+            stage.viewport.scale = .75;
+            stage.viewport.offsetX = -200;
+            stage.on("destroy",function() {
+                mario.destroy();
+            });
+        });
+
+        Q.stageScene("level1");
+
+    });
 }
-
-
